11409c56cd945e976a9a6def45886d60c26ef470
baekjae2-code <baekjae2@gmail.com>
2026-09-07 17:09:40 +0900

    식당 업글, 요리 취소, 음식 스케일, 골드 팝업, 레시피 해금

--- Assets/01.Scripts/Upgrade/GoldUI.cs
+++ Assets/01.Scripts/Upgrade/GoldUI.cs
@@ -25,7 +25,7 @@ public class GoldUI : MonoBehaviour
 
     private void OnGoldChanged()
     {
-        int gold = GameManager.instance.PlayerData.gold;
+        BigNumber gold = GameManager.instance.PlayerData.gold;
         goldText.text = $"Gold: {gold:N0}";
     }
 }
