c885a8ef2dd64d20a75b0d6399392214dbb6ea91
gyeommi <kkm417417@naver.com>
2026-09-08 18:20:36 +0900

    [업그레이드] 업글 성공/실패 팝업 구현

--- Assets/01.Scripts/Upgrade/UpgradeUI.cs
+++ Assets/01.Scripts/Upgrade/UpgradeUI.cs
@@ -12,6 +12,10 @@ public class UpgradeUI : MonoBehaviour
     [SerializeField] private Button upgradeButton;
     [SerializeField] private TMP_Text upgradeButtonText;
 
+    [Header("POPUP")]
+    [SerializeField] private GameObject successPopup;
+    [SerializeField] private GameObject failPopup;
+
     private void Start()
     {
         upgradeButton.onClick.AddListener(OnClickUpgrade);
@@ -39,9 +43,18 @@ public class UpgradeUI : MonoBehaviour
     {
         PlayerData playerData = GameManager.instance.PlayerData;
 
-        UpgradeManager.instance.TryUpgrade(upgradeData, playerData);
+        bool isSuccess = UpgradeManager.instance.TryUpgrade(upgradeData, playerData);
 
         RefreshUI();
+
+        if (isSuccess)
+        {
+            successPopup.SetActive(true);
+        }
+        else
+        {
+            failPopup.SetActive(true);
+        }
     }
 
     private void OnUpgradePurchased(UpgradeData data, int level)
